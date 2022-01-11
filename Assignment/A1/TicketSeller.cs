e11d7600f81c1352e2fe1c79850f897f4195c3bc
My2ndAngelic <8387105+My2ndAngelic@users.noreply.github.com>
2022-01-11 14:17:19 +0100

    Update some stuff

--- Assignment/A1/TicketSeller.cs
+++ Assignment/A1/TicketSeller.cs
@@ -16,7 +16,43 @@ namespace A1
 
         public void Start()
         {
+            ReadAndSaveTicket();
+            DisplayTicketPrice();
+        }
 
+        public void ReadAndSaveTickets()
+        {
+            ReadAndSaveName();
+            ReadAndSaveAdult();
+            ReadAndSaveChildren();
+        }
+
+        public void ReadAndSaveName()
+        {
+            Console.WriteLine("Your name please: ");
+            name = Console.ReadLine();
+        }
+
+        public void ReadAndSaveAdult()
+        {
+            Console.Write("How many adults? ");
+            numOfAdults = int.Parse(Console.ReadLine());
+        }
+
+        public void ReadAndSaveChildren()
+        {
+            Console.Write("How many children? ");
+            numOfChildren = int.Parse(Console.ReadLine());
+        }
+
+        public void CalculateTicketPrice()
+        {
+            amountToPay = 69420;
+        }
+
+        public void DisplayTicketPrice()
+        {
+            Console.WriteLine("ur mom");
         }
     }
 }
