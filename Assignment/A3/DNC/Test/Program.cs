d1d30131e577bd481968a9082628b641a2fcbdc7
My2ndAngelic <8387105+My2ndAngelic@users.noreply.github.com>
2022-02-03 01:19:11 +0100

    R

--- Assignment/A3/DNC/Test/Program.cs
+++ Assignment/A3/DNC/Test/Program.cs
@@ -8,7 +8,7 @@ namespace Test
         static void Main(string[] args)
         {
             BMICalc bmic = new BMICalc("John", 69, 1.69, UnitType.Metric);
-            BMRCalc bmrc = new BMRCalc();
+            BMRCalc bmrc = new BMRCalc(bmic, 69, true, 4);
             bmrc.SetName("John");
             Console.WriteLine(bmrc.ExtractName());
         }
