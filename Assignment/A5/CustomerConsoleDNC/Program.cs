f0fed3f83c622e0d8c42afd4b92bfa8172f032bc
My2ndAngelic <8387105+My2ndAngelic@users.noreply.github.com>
2022-03-08 19:04:54 +0100

    Test

--- Assignment/A5/CustomerConsoleDNC/Program.cs
+++ Assignment/A5/CustomerConsoleDNC/Program.cs
@@ -7,7 +7,7 @@ namespace CustomerConsoleDNC
         public static void Main(string[] args)
         {
             Console.Clear();
-            string a = Console.ReadLine();
+            string? a = Console.ReadLine();
             Console.WriteLine(a is "42069" or "69420" ? "Hello World" : "Goodbye World");
         }
     }
--- Assignment/A5/CustomerConsoleDNF/Program.cs
+++ Assignment/A5/CustomerConsoleDNF/Program.cs
@@ -7,10 +7,13 @@ namespace CustomerConsoleDNF
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine(Countries.Antigua_and_Barbuda.ToString().Replace("_", " "));
-            Console.WriteLine(new Email("a@a.com").IsValidPersonalMail());
-            Console.ReadLine();
+            // Console.WriteLine("Hello, World!");
+            // Console.WriteLine(Countries.Antigua_and_Barbuda.ToString().Replace("_", " "));
+            // Console.WriteLine(new Email("a@a.com").IsValidPersonalMail());
+            // Console.ReadLine();
+            Console.Clear();
+            string a = Console.ReadLine();
+            Console.WriteLine(a == "42069" || a == "69420" ? "Hello World" : "Goodbye World");
         }
     }
 }
